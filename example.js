0867f620c940db41179a26bf16206b66a02a0b57
Sirichai Chulee <62983503+jsirichai@users.noreply.github.com>
2024-04-16 17:10:14 +0700

    Update example.js

--- example.js
+++ example.js
@@ -3,7 +3,7 @@ const PGW2C2P = require("./src/lib/PGW2C2P");
 const pgw = new PGW2C2P({
   secretKey: "CD229682D3297390B9F66FF4020B758F4A5E625AF4992E5D75D311D6458B38E2",
   merchantID: "JT04",
-  environment: "sandbox", // Environment (sandbox/live) defaults to sandbox.
+  environment: "sandbox", // Environment (sandbox/live).
   apiVersion: "4.3", // default to 4.3.
   debug: true, // default to true.
 });
